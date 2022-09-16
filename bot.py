60c582286d038f2e87f914c9631872c0bd71b9ec
Stephen McAleese <stephenmcaleese11@gmail.com>
2022-09-16 20:07:09 +0100

    added daily quote method

--- bot.py
+++ bot.py
@@ -1,5 +1,7 @@
 import tweepy
 from dotenv import dotenv_values
+from datetime import datetime
+import json
 
 class TwitterBot:
     def __init__(self, config):
@@ -8,11 +10,22 @@ class TwitterBot:
     def tweet(self, message):
         self.client.create_tweet(text=message)
 
+    def get_todays_quote(self):
+        with open('quotes.json') as f:
+            quotes_list = json.load(f)['quotes']
+        now = datetime.now()
+        day_num = now.strftime('%j')
+        index = int(day_num) % len(quotes_list)
+        return quotes_list[index]
+
+    def tweet_todays_quote(self):
+        quote = self.get_todays_quote()
+        self.tweet(quote)
+
 def main():
     config = dotenv_values('.env')
     bot = TwitterBot(config)
-    message = 'hello world'
-    bot.tweet(message)
+    bot.tweet_todays_quote()
 
 if __name__ == '__main__':
     main()
